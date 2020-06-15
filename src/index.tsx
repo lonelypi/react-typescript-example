600e520237b0d8945677f7967d8204b0db054bde
Pedro Ivo de Faria Ramos <pedro.ramos@embraer.com.br>
2020-06-15 12:21:08 -0300

    App feito em TS do curso de TS do Stephen Grider

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import { reducers } from './reducers'
+import { App } from './components'
+
+    
+const store = createStore(reducers, applyMiddleware(thunk))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const App = (props: AppProps):JSX.Element => {
+//     return <div className='ui container'>{props.color}</div>
+// }
+
+// interface AppProps {
+//     color?: string
+// }
+
+// class App extends React.Component<AppProps> {
+
+//     //With react and typescript we do not define a countructor to define the state!!
+//     state = { counter: 0 }
+//     //
+
+//     onIncrement = ():void => {
+//          this.setState({counter: this.state.counter + 1})
+//     }
+
+//     onDecrement = ():void => {
+//          this.setState({counter: this.state.counter - 1})
+//      }
+
+//     render(){
+//         return (
+//             <div className='ui container'>
+//                  <button onClick={this.onIncrement} className='ui button primary'>Increment</button>
+//                  <button onClick={this.onDecrement} className='ui button negative'>Decrement</button>
+//                  <br />
+//                  {this.state.counter}
+//             </div>
+//          )
+//     }
+// }
